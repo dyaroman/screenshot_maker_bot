18a4c1b7dbf722131a4ed6eb85614536390dd14b
Roman Diachenko <romand@leadsmarket.com>
2019-08-12 16:53:25 +0300

    refactoring

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ const dotenv = require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = process.env.TOKEN;
 const bot = new TelegramBot(TOKEN, { polling: true });
-
 const puppeteer = require('puppeteer');
-
 const fs = require('fs');
-const screenshotsDir = 'screenshots';
 
 
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'run /screenshot command with url to make a screenshot');
+const help = chatId => {
+  bot.sendMessage(chatId, `Please run command /screenshot with URL parameter.
+  Examples:
+  "/screenshot https://google.com" - I will send you screenshot of the full page.
+  "/screenshot https://google.com .content" - I will send you screenshot of '.content' block.
+  `);
+};
+
+bot.onText(/\/start/, msg => {
+  help(msg.chat.id);
 });
 
-bot.onText(/\/screenshot (.+) (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const url = match[1];
-  const cssSelector = match[2];
+bot.onText(/\/help/, msg => {
+  help(msg.chat.id);
+});
+
+const makeScreenshot = (chatId, url, selector) => {
+  const screenshotsDir = 'screenshots';
   const screenshotName = new Date().getTime();
 
   // check exist screenshots directory or create one
@@ -27,7 +34,7 @@ bot.onText(/\/screenshot (.+) (.+)/, (msg, match) => {
   (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    
+
     try {
       await page.goto(url, {
         timeout: 5 * 60 * 1000,
@@ -37,10 +44,10 @@ bot.onText(/\/screenshot (.+) (.+)/, (msg, match) => {
       bot.sendMessage(chatId, err.message);
       return;
     }
-    
-    const el = await page.$(cssSelector);
-    
-    if (cssSelector && el) {
+
+    const el = await page.$(selector);
+
+    if (selector && el) {
       await el.screenshot({
         path: `./${screenshotsDir}/${screenshotName}.png`
       });
@@ -67,4 +74,31 @@ bot.onText(/\/screenshot (.+) (.+)/, (msg, match) => {
       console.error(err);
     }
   })();
+};
+
+bot.on('message', msg => {
+  const text = msg.text.trim().replace(/  +/gm, ' ');
+  const chatId = msg.chat.id;
+  const parameters = text.split(' ');
+  const command = parameters[0];
+  const url = parameters[1];
+  const selector = parameters[2];
+
+  if (command !== '/screenshot') {
+    return;
+  }
+
+  switch (parameters.length) {
+    case 2:
+      //comand, url
+      makeScreenshot(chatId, url);
+      break;
+    case 3:
+      //command, url, selector
+      makeScreenshot(chatId, url, selector);
+      break;
+    default:
+      // no parameters
+      help(chatId);
+  }
 });
