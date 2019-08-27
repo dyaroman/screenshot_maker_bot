d5b40b9690435497d28fb0773473376e8a9e2090
Roman Diachenko <romand@leadsmarket.com>
2019-08-27 16:57:44 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,104 +1,88 @@
-const TelegramBot = require('node-telegram-bot-api');
+const Telegraf = require('telegraf');
 const puppeteer = require('puppeteer');
-const fs = require('fs');
 require('dotenv').config();
 
-const bot = new TelegramBot(process.env.TOKEN, { polling: true });
+const bot = new Telegraf(process.env.TOKEN);
 
 
-const help = chatId => {
-  bot.sendMessage(chatId, `Please run command /screenshot with URL parameter.
-  Examples:
-  "/screenshot https://google.com" - I will send you screenshot of the full page.
-  "/screenshot https://google.com .content" - I will send you screenshot of '.content' block.
-  `);
-};
-
-bot.onText(/\/start/, msg => {
-  help(msg.chat.id);
+bot.help((ctx) => {
+  help(ctx);
 });
 
-bot.onText(/\/help/, msg => {
-  help(msg.chat.id);
+bot.start((ctx) => {
+  help(ctx);
 });
 
-const makeScreenshot = (chatId, url, selector) => {
-  const screenshotsDir = 'screenshots';
-  const screenshotName = new Date().getTime();
-
-  // check exist screenshots directory or create one
-  if (!fs.existsSync(screenshotsDir)) {
-    fs.mkdirSync(screenshotsDir);
-  }
-
-  (async () => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox']});
-    const page = await browser.newPage();
-
-    try {
-      await page.goto(url, {
-        timeout: 5 * 60 * 1000,
-        waitUntil: 'load'
-      });
-    } catch (err) {
-      bot.sendMessage(chatId, err.message);
-      return;
-    }
-
-    const el = await page.$(selector);
-
-    if (selector && el) {
-      await el.screenshot({
-        path: `./${screenshotsDir}/${screenshotName}.png`
-      });
-    } else {
-      await page.screenshot({
-        path: `./${screenshotsDir}/${screenshotName}.png`,
-        fullPage: true
-      });
-    }
-
-    await browser.close();
-
-    const screenshot = `${__dirname}/${screenshotsDir}/${screenshotName}.png`;
-
-    // check exsit screenshot and send it
-    if (fs.existsSync(screenshot)) {
-      await bot.sendPhoto(chatId, screenshot);
-    }
-
-    // remove sent screenshot
-    try {
-      fs.unlinkSync(screenshot);
-    } catch (err) {
-      console.error(err);
-    }
-  })();
-};
-
-bot.on('message', msg => {
-  const text = msg.text.trim().replace(/  +/gm, ' ');
-  const chatId = msg.chat.id;
+bot.on('text', ctx => {
+  const text = ctx.message.text.trim().replace(/  +/gm, ' ');
   const parameters = text.split(' ');
-  const command = parameters[0];
-  const url = parameters[1];
-  const selector = parameters[2];
+  const COMMAND = parameters[0];
+  const URL = parameters[1];
+  const SELECTOR = parameters[2];
 
-  if (command !== '/screenshot') {
+  if (COMMAND !== '/screenshot') {
     return;
   }
 
   switch (parameters.length) {
     case 2:
       //comand, url
-      makeScreenshot(chatId, url);
+      makeScreenshot(ctx, URL);
       break;
     case 3:
       //command, url, selector
-      makeScreenshot(chatId, url, selector);
+      makeScreenshot(ctx, URL, SELECTOR);
       break;
     default:
       // no parameters
-      help(chatId);
+      help(ctx);
   }
 });
+
+async function makeScreenshot(ctx, URL, SELECTOR) {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+  const page = await browser.newPage();
+  let screenshot;
+
+  try {
+    await page.goto(URL, {
+      timeout: 5 * 60 * 1000,
+      waitUntil: 'load'
+    });
+  } catch (err) {
+    ctx.reply(err.message);
+    return;
+  }
+
+  const el = await page.$(SELECTOR);
+
+  if (SELECTOR && el) {
+    screenshot = await el.screenshot({
+      omitBackground: true,
+      encoding: 'binary'
+    });
+  } else {
+    screenshot = await page.screenshot({
+      omitBackground: true,
+      encoding: 'binary',
+      fullPage: true
+    });
+  }
+
+  await browser.close();
+
+  await ctx.replyWithPhoto({
+    source: screenshot
+  });
+};
+
+function help(ctx) {
+  ctx.reply(`
+Please run command /screenshot with URL parameter.\n
+Examples:
+"/screenshot https://google.com" - I will send you screenshot of the full page.\n
+"/screenshot https://google.com .content" - I will send you screenshot of ".content" block.
+`);
+}
+
+bot.startPolling();
